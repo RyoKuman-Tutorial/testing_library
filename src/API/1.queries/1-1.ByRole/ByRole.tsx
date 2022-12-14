f02450e12a15a556df9a198778d93e87647db8e7
Ryokuman <ryokuman@naver.com>
2022-12-14 23:55:52 +0900

    feature ( component ) : add ByRole

---
+++ src/API/1.queries/1-1.ByRole/ByRole.tsx
@@ -0,0 +1,27 @@
+function ByRole() {
+  return (
+    <div>
+      <div role="div1">
+        <h1>div1H1</h1>
+      </div>
+      <div role="div2">
+        <h1>div2H1</h1>
+      </div>
+      <div role="div3">
+        <input type="text" placeholder="textInput" />
+        <input type="button" value="button" />
+      </div>
+      <div role="div4">
+        <p role="ppp">p tag</p>
+      </div>
+      <div role="div5">
+        <h1>h1</h1>
+        <h2>h2</h2>
+        <h3>h3</h3>
+        <h4>h4</h4>
+      </div>
+    </div>
+  );
+}
+
+export default ByRole;
