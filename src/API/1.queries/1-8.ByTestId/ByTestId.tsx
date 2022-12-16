25530093a51770bc542099eb2951131bf3ebbfda
Ryokuman <ryokuman@naver.com>
2022-12-16 20:14:30 +0900

    feature ( component ) : add ByTestId.tsx

---
+++ src/API/1.queries/1-8.ByTestId/ByTestId.tsx
@@ -0,0 +1,11 @@
+function ByTestId() {
+  return (
+    <div>
+      <h1 data-testid="h1">h1</h1>
+      <input type="text" data-testid="input" placeholder="input" />
+      <button value="button" data-testid="button" />
+    </div>
+  );
+}
+
+export default ByTestId;
