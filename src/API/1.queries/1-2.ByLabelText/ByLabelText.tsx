882f8b07e19288f55db08b1de16044ffdb1fb799
Ryokuman <ryokuman@naver.com>
2022-12-16 18:19:49 +0900

    feature ( component ) : add ByLabelText.tsx

--- src/API/1.queries/1-2.ByLabelText/ByLabelText.tsx
+++ src/API/1.queries/1-2.ByLabelText/ByLabelText.tsx
@@ -1,5 +1,24 @@
 function ByLabelText() {
-  return <div />;
+  return (
+    <div>
+      <div role="div1">
+        <label htmlFor="cheese">Do you like cheese?</label>
+        <input type="text" id="cheese" placeholder="cheese" />
+        <label htmlFor="bread">Do you like bread?</label>
+        <input type="text" id="bread" placeholder="bread" />
+      </div>
+      <div role="div2">
+        <label>
+          textInput
+          <input type="text" placeholder="this is input" />
+        </label>
+        <label>
+          textInput
+          <textarea placeholder="this is textarea" />
+        </label>
+      </div>
+    </div>
+  );
 }
 
 export default ByLabelText;
