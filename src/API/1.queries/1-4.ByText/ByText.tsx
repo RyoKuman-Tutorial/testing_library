f481f689a365dfc6d4660ccf83f04d28f5c312e9
Ryokuman <ryokuman@naver.com>
2022-12-16 20:29:12 +0900

    feature ( 1-4.ByText ) : component
    ByText.tsx
     - 예제 추가 ( options : ignore )

--- src/API/1.queries/1-4.ByText/ByText.tsx
+++ src/API/1.queries/1-4.ByText/ByText.tsx
@@ -5,6 +5,11 @@ function ByText() {
       <p>p</p>
       <span>span</span>
       <input type="button" value="button" />
+
+      <div role="ignore">
+        <p id="p">ignore</p>
+        <span id="span">ignore</span>
+      </div>
     </div>
   );
 }
